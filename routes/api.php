1923e7a8347c29855fff0bd2f2213de9f10dfe9c
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-09-16 21:12:21 +0330

    2112

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,11 @@ use App\Http\Controllers\UsersController;
 Route::prefix('/user')->group(function () {
 
     Route::put('/signup', [UsersController::class, 'signup']);
-    Route::post('/login', [UsersController::class, 'login']);
 
     Route::middleware(['auth:sanctum'])->group(function () {
 
         Route::post('/logout', [UsersController::class, 'logout']);
-        Route::get('/profile', [UsersController::class, 'profile']);
+        Route::post('/profile', [UsersController::class, 'profile']);
         Route::post('/update-profile', [UsersController::class, 'update']);
         Route::post('/upload-image', [UsersController::class, 'uploadImage']);
         Route::post('/upload-cover', [UsersController::class, 'uploadCover']);
