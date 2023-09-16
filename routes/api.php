34adb2931450ccce0638207c651923425379f741
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-09-16 17:21:01 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/signup', [UsersController::class, 'signup']);
+Route::put('/signup', [UsersController::class, 'signup']);
 
 
