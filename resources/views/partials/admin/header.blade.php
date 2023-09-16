9b43d1d76e51808b8fe9907d9b28a586e892fd39
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-09-16 17:50:48 +0200

    qrwr

--- resources/views/partials/admin/header.blade.php
+++ resources/views/partials/admin/header.blade.php
@@ -72,7 +72,7 @@
         </div>
         <div class="ms-auto">
             <ul class="list-unstyled">
-                @if( \Auth::user()->type !='client' && \Auth::user()->type !='super admin' )
+{{--                @if( \Auth::user()->type !='client' && \Auth::user()->type !='super admin' )--}}
                     <li class="dropdown dash-h-item drp-notification">
                         <a class="dash-head-link arrow-none me-0" href="{{ url('chats') }}" aria-haspopup="false"
                            aria-expanded="false">
@@ -82,7 +82,7 @@
 {{--                            </span>--}}
 {{--                        </a>--}}
                     </li>
-                @endif
+{{--                @endif--}}
 
 
 
