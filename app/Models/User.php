0d4cec1bf7f64db32f1d175b596ea78781e71c4b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-09-16 18:53:20 +0330

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+
+class User extends Authenticatable
 {
+    use Notifiable;
     use HasFactory;
-    
+
     protected $fillable = ['name', 'email', 'password', 'eth_address','bio'];
 
     public function getImageUrlAttribute($value)
