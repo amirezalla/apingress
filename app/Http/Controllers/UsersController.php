bbd66fe1375c0db11e4a019f35255a39b30e5af2
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2023-09-16 20:13:27 +0330

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -43,13 +43,6 @@ class UsersController extends BaseController
     {
         $eth_address = $request->eth_address;
 
-        $credentials = $request->only(['eth_address']);
-
-        if (!Auth::attempt($credentials)) {
-            throw ValidationException::withMessages([
-                'eth_address' => ['The provided credentials are incorrect.'],
-            ]);
-        }
 
         $user = User::where('eth_address', $eth_address)->first();
 
